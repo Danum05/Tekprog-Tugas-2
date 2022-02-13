b86169df9b0b7b8c778ace346f1aad4d6abd2d1c
Danum05 <99567626+Danum05@users.noreply.github.com>
2022-02-13 20:55:14 +0700

    Add files via upload

---
+++ Constants/Constants.java
@@ -0,0 +1,27 @@
+package Constants;
+/**
+ * <h1>Soal 3: Constants</h1>
+ * Constants program implements an application
+ * to convert a paper size from inches to centimeters
+ * 
+ * @author Danu Mahesa
+ * @version 1.0
+ * @since 2022-02-12
+ */
+
+public class Constants { 
+    	/**
+	 * This is the main method.
+	 * Here's where the number of inches is 
+	 * multiplied by centimeters per inch
+	 * and turns into centimeters
+	 */
+public static void main(String[] args) { 
+ final double CM_PER_INCH = 2.54; 
+ double paperWidth = 8.5; 
+ double paperHeight = 11; 
+ System.out.println("Paper size in centimeters: " + 
+ paperWidth * CM_PER_INCH + " by " + paperHeight * 
+ CM_PER_INCH); 
+} 
+}
---
+++ Constants/Constants2.java
@@ -0,0 +1,26 @@
+package Constants;
+/**
+ * <h1>Soal 3: Constants</h1>
+ * Constants program implements an application
+ * to convert a paper size from inches to centimeters
+ * 
+ * @author Danu mahesa
+ * @version 1.0
+ * @since 2022-02-12
+ */
+
+ public class Constants2 { 
+ 	/**
+	 * This is the main method.
+	 * Here's where the number of inches is 
+	 * multiplied by centimeters per inch
+	 * and turns into centimeters
+	 */   
+ public static final double CM_PER_INCH = 2.54; 
+ public static void main(String[] args) {
+ double paperWidth = 8.5; 
+ double paperHeight = 11; 
+ System.out.println("Paper size in centimeters: " + paperWidth *
+ CM_PER_INCH + " by " + paperHeight * CM_PER_INCH); 
+ } 
+}
